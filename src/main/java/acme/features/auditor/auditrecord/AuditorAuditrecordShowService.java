9d5d9670e49c04657dcc76a0b234c858a260978e
Usuario <Usuario@dircom.manten.us.es>
2019-12-02 12:53:52 +0100

    He hecho que se listen y muestren los audit records en auditor.

---
+++ src/main/java/acme/features/auditor/auditrecord/AuditorAuditrecordShowService.java
@@ -0,0 +1,60 @@
+
+package acme.features.auditor.auditrecord;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.auditrecords.Auditrecord;
+import acme.entities.roles.Auditor;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Principal;
+import acme.framework.services.AbstractShowService;
+
+@Service
+public class AuditorAuditrecordShowService implements AbstractShowService<Auditor, Auditrecord> {
+
+	@Autowired
+	AuditorAuditrecordRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<Auditrecord> request) {
+		assert request != null;
+
+		boolean result;
+		int AuditrecordId;
+		Auditrecord Auditrecord;
+		Auditor auditor;
+		Principal principal;
+
+		AuditrecordId = request.getModel().getInteger("id");
+		Auditrecord = this.repository.findOneAuditrecordById(AuditrecordId);
+		auditor = Auditrecord.getJob().getAuditor();
+		principal = request.getPrincipal();
+		result = Auditrecord.getJob().isFinalMode() || !Auditrecord.getJob().isFinalMode() && auditor.getId() == principal.getActiveRoleId();
+		return result;
+	}
+
+	@Override
+	public void unbind(final Request<Auditrecord> request, final Auditrecord entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "title", "status", "moment", "body", "job");
+	}
+
+	@Override
+	public Auditrecord findOne(final Request<Auditrecord> request) {
+		assert request != null;
+
+		Auditrecord result;
+		int id;
+
+		id = request.getModel().getInteger("id");
+		result = this.repository.findOneAuditrecordById(id);
+		return result;
+	}
+
+}
--- src/main/webapp/META-INF/views/auditor/job/form.jsp
+++ src/main/webapp/META-INF/views/auditor/job/form.jsp
@@ -10,6 +10,9 @@
 	<acme:form-money code="auditor.job.form.label.salary" path="salary"/>
 	<acme:form-money code="auditor.job.form.label.moreInfo" path="moreInfo"/>
 	<acme:form-textbox code="auditor.job.form.label.description" path="description"/>
+	<jstl:set var="id" value="${id}"/>
+	<h4><acme:menu-suboption code="auditor.auditrecord.form.label.listAuditrecord" action="/auditor/auditrecord/list_mine?id=${id}"/></h4>
+	
 	
 	<acme:form-return code="auditor.job.form.label.button.return"/>
 </acme:form>
